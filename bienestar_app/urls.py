f48440468d7899e84e739b9fbacb22c2909cb3ae
Efrain-DiazM <efradiaz@gmail.com>
2024-10-05 22:22:14 -0500

    documentacion swagger

--- bienestar_app/urls.py
+++ bienestar_app/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentacion de API",
+      default_version='v0.1',
+      description="Documentacion publica de API Bienestar UCC",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.jason/\.yamL)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     # Include the urls from the users app
     path('users/', include('apps.users.api.urls')),
